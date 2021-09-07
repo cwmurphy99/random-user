8dbfd4be992b6950abc290f2f060e0a64b28cf2e
Chris Murphy <theworldisyours@gmail.com>
2021-09-07 15:22:46 -0400

    added a button to allow a new user to be shown without reloading the page

--- scripts/data/DataManager.js
+++ scripts/data/DataManager.js
@@ -7,7 +7,7 @@ export const getRandomUser = () => {
     return fetch('https://randomuser.me/api') 
     .then(response => response.json())
     .then(parsedResponse => {
-        console.log("user info: ", parsedResponse)
+        //console.log("user info: ", parsedResponse)
         userCollection = parsedResponse.results
         return parsedResponse;
     })
